468d8819aaf213dcd886247d0e36e4a9a7b1fb82
russgray <russgray@gmail.com>
2016-08-18 12:26:29 +0100

    Better x-axis labels

--- index.js
+++ index.js
@@ -2,140 +2,142 @@ var d3 = require('d3');
 
 function BarChart(id, data, options) {
     var cfg = {
-        w: 500,                //Width of the circle
-        h: 500,                //Height of the circle
-        margin: 10,            //The margins of the SVG
-        layout: false,
-        color: d3.scale.category10()   //Color function
+        w: 500,                         //Width of the circle
+        h: 500,                         //Height of the circle
+        margin: 20,                     //The margins of the SVG
+        color: d3.scale.category10(),   //Color function
+        labels: d3.range(data[0].length),
+        tooltips: false                 //Show tooltips
     };
 
-    //Put all of the options into a variable called cfg
+    // Put all of the options into a variable called cfg
     if ('undefined' !== typeof options) {
         for (var i in options) {
             if('undefined' !== typeof options[i]) { 
                 cfg[i] = options[i]; 
             }
-        }//for i
-    }//if
-
-    function barStack(d) {
-        var l = d[0].length
-        while (l--) {
-            var posBase = 0, negBase = 0;
-            d.forEach(function(d) {
-                d=d[l]
-                d.size = Math.abs(d.y)
-                if (d.y<0)  {
-                    d.y0 = negBase
-                    negBase-=d.size
-                } else
-                {
-                    d.y0 = posBase = posBase + d.size
-                }
-            })
         }
-        d.extent= d3.extent(d3.merge(d3.merge(d.map(function(e) { return e.map(function(f) { return [f.y0,f.y0-f.size]})}))))
-        return d
     }
 
-    var  h=cfg.h
-        ,w=cfg.w
-        ,margin=cfg.margin
-        ,color = cfg.color
+    function barStack(seriesData) {
+        var l = seriesData[0].length;
+        while (l--) {
+            var posBase = 0; // positive base
+            var negBase = 0; // negative base
 
-        ,x = d3.scale.ordinal()
-            .domain(d3.range(data[0].length+1))
-            .rangeRoundBands([margin,w-margin], .1)
+            seriesData.forEach(function(d) {
+                d = d[l];
+                d.size = Math.abs(d.y);
+                if (d.y < 0) {
+                    d.y0 = negBase;
+                    negBase -= d.size;
+                } else {
+                    d.y0 = posBase = posBase + d.size;
+                }
+            });
+        }
+        seriesData.extent = d3.extent(
+            d3.merge(
+                d3.merge(
+                    seriesData.map(function(e) { 
+                        return e.map(function(f) { 
+                            return [Math.ceil(f.y0), Math.floor(f.y0 - f.size)];
+                        });
+                    })
+                )
+            )
+        );
+        console.log('extent: ' + seriesData.extent);
+    }    
 
-        ,y = d3.scale.linear()
-            .range([h-margin,0+margin])
+    var h = cfg.h;
+    var w = cfg.w;
+    var margin = cfg.margin;
+    var color = cfg.color;
+    var labels = cfg.labels;
 
-        ,xAxis = d3.svg.axis().scale(x).orient("bottom").tickSize(6, 0)
-        ,yAxis = d3.svg.axis().scale(y).orient("left")
+    var x = d3.scale.ordinal()
+        .domain(labels)
+        .rangeRoundBands([margin, w - margin], .1);
 
-    barStack(data)
-    y.domain(data.extent)
+    var y = d3.scale.linear()
+        .range([h-margin, 0+margin]);
 
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .tickSize(6, 0);
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    barStack(data);
+    y.domain(data.extent);
 
     svg = d3.select("body")
         .append("svg")
-        .attr("height",h)
-        .attr("width",w)
+        .attr("height", h)
+        .attr("width", w);
 
-    svg.selectAll(".series").data(data)
-        .enter().append("g").classed("series",true).style("fill", function(d,i) { return color(i)})
-            .selectAll("rect").data(Object)
-            .enter().append("rect")
+    svg.selectAll(".series")
+        .data(data)
+        .enter()
+        .append("g")
+        .classed("series", true)
+        .style("fill", function(d,i) { return color(i); })
+        .style("opacity", 0.8)
+            .selectAll("rect")
+            .data(Object)
+            .enter()
+            .append("rect")
+                .attr("x", function(d, i) { return x(x.domain()[i]); })
+                .attr("y", function(d) { return y(d.y0); })
+                .attr("height", function(d) { return y(0) - y(d.size); })
+                .attr("width", x.rangeBand())
+                .on("mouseover", function() { if (cfg.tooltips) { tooltip.style("display", null); } })
+            .on("mouseout", function() { 
+                if (cfg.tooltips) { tooltip.style("display", "none") }; 
+            })
+            .on("mousemove", function(d) {
+                if (cfg.tooltips) {
+                    var xPosition = d3.mouse(this)[0] - 35;
+                    var yPosition = d3.mouse(this)[1] - 5;
+                    tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+                    tooltip.select("text").text(d.y);
+                }
+            });
 
-    svg.append("g").attr("class","axis x")
-    svg.append("g").attr("class","axis y")
+    svg.append("g")
+        .attr("class", "axis x")
+        .attr("transform", "translate(0 " + y(0) + ")")
+        .call(xAxis);
 
+    svg.append("g")
+        .attr("class", "axis y")
+        .attr("transform", "translate(" + margin + " 0)")
+        .call(yAxis);
 
-    var layout = 0,dur=0
-    redraw()
-    dur = 1500
+    /* Here we add tooltips */
+    if (cfg.tooltips) {
+        // Prep the tooltip bits, initial display is hidden
+        var tooltip = svg.append("g")
+            .attr("class", "tooltip")
+            .style("display", "none");
+            
+        tooltip.append("rect")
+            .attr("width", 40)
+            .attr("height", 30)
+            .attr("fill", "white")
+            .style("opacity", 0.5);
 
-
-    function redraw() {
-        if (layout=!layout) {
-            /* Readjust the range to witdh and height */
-            x.rangeRoundBands([margin,w-margin], .1)
-            y.range([h-margin,0+margin])
-
-            /* Reposition and redraw axis */
-            svg.select(".x.axis")
-                .transition().duration(dur)
-                .attr("transform","translate (0 "+y(0)+")")
-                .call(xAxis.orient("bottom"))
-            svg.select(".y.axis")
-                .transition().duration(dur)
-                .attr("transform","translate ("+x(0)+" 0)")
-                .call(yAxis.orient("left"))
-
-            /* Reposition the elements */
-            svg.selectAll(".series rect")
-                .transition().duration(dur)
-                .attr("x",function(d,i) { return x(d.x)})
-                .attr("y",function(d) { return y(d.y0)})
-                .attr("height",function(d) { return y(0)-y(d.size)})
-                .attr("width",x.rangeBand())
-
-        } else {
-            /* Readjust the range to witdh and height */
-            x.rangeRoundBands([h-margin,0+margin], .1)
-            y.range([margin,w-margin])
-
-            /* Reposition and redraw axis */
-            svg.select(".x.axis")
-                .transition().duration(dur)
-                .attr("transform","translate ("+y(0)+" 0)")
-                .call(xAxis.orient("left"))
-            svg.select(".y.axis")
-                .transition().duration(dur)
-                .attr("transform","translate (0 "+x(0)+")")
-                .call(yAxis.orient("bottom"))
-
-            /* Reposition the elements */
-            svg.selectAll(".series rect")
-                .transition().duration(dur)
-                .attr("y",function(d,i) { return x(d.x)})
-                .attr("x",function(d) { return y(d.y0-d.size)})
-                .attr("width",function(d) { return y(d.size)-y(0)})
-                .attr("height",x.rangeBand())
-
-        }
-
-    if (cfg.layout) {
-        d3.select("body").append("button")
-            .attr("type","button")
-            .text("Change Layout")
-            .style("position","absolute")
-            .style("left","5px")
-            .style("top","5px")
-            .on("click",redraw)
-        }
+        tooltip.append("text")
+            .attr("x", 20)
+            .attr("dy", "1.5em")
+            .style("text-anchor", "middle")
+            .attr("font-size", "12px")
+            .attr("font-weight", "bold");
     }
-
 }
 
 module.exports = BarChart;
